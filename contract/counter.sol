66ce35d536ae7966636b030dc18a5c5cdd68a2a4
Muthomi888 <145975558+Muthomi888@users.noreply.github.com>
2024-11-18 21:02:54 +0300

    complete Dapp

---
+++ contract/counter.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.0;
+
+contract BITKCA {
+    //declaring the state variables
+    uint256 number;
+    string public message;
+
+    //constructors
+    constructor(uint256 startingPoint, string memory startingMessage){
+        number = startingPoint;
+        message = startingMessage; 
+    }
+
+
+    //reading function
+    function getNumber() external view returns(uint256){
+        return number;
+    }
+
+    //writing functions 
+    //increasing the number by one
+    function increaseNumber() external {
+        number++;
+    }
+
+    //decreasing the number by 1
+    function decreaseNumber() external {
+        number--;
+    }
+
+    //function to update the message
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+    
+
+    
+}
